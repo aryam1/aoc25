c98218f10f2e42df2c59640457601ea000c2bf30
Arya Mukherjee <arya.mukherjee@sita.aero>
2025-12-03 00:30:23 +0000

    convoluted logic which seems right but just doesnt work

--- day1.cpp
+++ day1.cpp
@@ -5,17 +5,30 @@
 int main() {
     std::ifstream file("d1key.txt");
     std::string line;
-    int pos[100]{};
+    int part1 = 0;
+    int part2 = 0;
     int ptr = 50;
 
     while (std::getline(file, line)) {
-	int sclr = std::stoi(line.substr(1));
-	if (line[0] == 'L') sclr = -sclr;
-	ptr +=sclr;
-	while (ptr < 0) ptr += 100;
-	while (ptr > 99) ptr -= 100;
-	pos[ptr] += 1;
-    }
-    std::cout << pos[0] << std::endl;
+		int sclr = std::stoi(line.substr(1));
+		if (line[0] == 'L') sclr = -sclr;
+		int og = ptr;
+		ptr += sclr;
+		int dv = ptr/100;
+		int rem = ptr % 100;
+		if (rem < 0) {
+			dv = 1-dv;
+			rem+=100;
+		}
+		ptr = rem;
+		if (og == 0 && dv > 0) dv--;
+		if (ptr == 0) {
+			part1++;
+			if (dv > 0) dv--;
+		}
+		part2 += dv;
+	}
+    std::cout << part1 << ' ' << part2 << std::endl;
+    std::cout << part1+part2<<std::endl;
     return 0;
 }
