396c5c6005112a52ba774ae6086c51ba8e7e214b
Arya Mukherjee <arya.mukherjee@sita.aero>
2025-12-03 00:35:37 +0000

    super simple brute force that im not happy with but works

--- day1.cpp
+++ day1.cpp
@@ -5,30 +5,17 @@
 int main() {
     std::ifstream file("d1key.txt");
     std::string line;
-    int part1 = 0;
-    int part2 = 0;
+    int ans = 0;
     int ptr = 50;
 
     while (std::getline(file, line)) {
 		int sclr = std::stoi(line.substr(1));
-		if (line[0] == 'L') sclr = -sclr;
-		int og = ptr;
-		ptr += sclr;
-		int dv = ptr/100;
-		int rem = ptr % 100;
-		if (rem < 0) {
-			dv = 1-dv;
-			rem+=100;
+		for (int i =0; i <sclr; i++){
+			if (line[0] == 'L') ptr = (ptr-1+100)%100;
+			else ptr=(ptr+1)%100;
+			if (ptr == 0) ans++;
 		}
-		ptr = rem;
-		if (og == 0 && dv > 0) dv--;
-		if (ptr == 0) {
-			part1++;
-			if (dv > 0) dv--;
-		}
-		part2 += dv;
 	}
-    std::cout << part1 << ' ' << part2 << std::endl;
-    std::cout << part1+part2<<std::endl;
+	std::cout << ans;
     return 0;
 }
