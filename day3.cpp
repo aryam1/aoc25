182f9d4749ad2abd92d5d3f56517e41c782db2f3
Arya Mukherjee <arya.mukherjee@sita.aero>
2025-12-04 00:04:23 +0000

    day 3 fully finished, actually on the given day for once

--- day3.cpp
+++ day3.cpp
@@ -8,14 +8,34 @@ int main() {
     std::string bank;
     long long total = 0;
     while (std::getline(file, bank)) {
-    	cout << bank << '\n';
     	vector<int> v;
-   	    transform(bank.begin(), bank.end()-1, back_inserter(v), [](char c) { return c - '0'; });
-   	    vector<int>::iterator max1, max2;
-   	    max1 = max_element(v.begin(),v.end());
-   	    max2 = max_element(max1+1,v.end());
-   	    cout << *max1 << *max2<< '\n';
-   	    
+   	    for (char c : bank) {
+   	        if (isdigit((unsigned char)c)) v.push_back(c - '0');
+   	    }
+   	    // Logic now is given some N and string len X, buffer is
+   	    // X -(N-1), so each digit has the following buffer of numbers
+   	    // to max from, leaving enough digits for remaining N, 
+   	    // this buffer is reduced by how far into its buffer region
+   	    // a digit is selected from, as all following numbers 
+   	    // lose this same amount of buffer 
+   	    vector<int>::iterator prev, curr;
+   	    const int batts = 12;
+   	    int buffer = v.size() - (batts-1);
+   	    prev = v.begin();
+   	    array<int,batts> val{};
+   	    //cout << bank << '\n';
+   	    for (int i = 0; i < batts; ++i){
+   	    	curr = max_element(prev,prev+buffer);
+   	    	//cout <<string((int)(curr-prev),' ')<<"^";
+   	    	val[i] = *curr;
+   	    	buffer -= (curr-prev);
+   	    	prev = curr+1;
+   	    }
+   	    string valstr;
+   	    for (int x : val) valstr += '0' + x;
+   	    total += stoll(valstr);
+   	    //print("Val: {}\n",valstr);
     }
+    print("Joltage Total: {}\n", total);
     return 0;
 }
