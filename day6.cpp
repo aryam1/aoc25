255841c5aba4b5738c1fc4eeac1402344538e415
Arya Mukherjee <arya@hotmail.com>
2025-12-07 00:24:09 +0000

    Removed some faulty logic for part 2, will tackle tomorrow

--- day6.cpp
+++ day6.cpp
@@ -11,25 +11,19 @@ int main() {
 	std::ifstream file("d6key.txt");
 	std::string row;
    	std::string seg;
-	std::vector<std::vector<std::string>> nums;
-	std::vector<std::string> ops;
-	// Biggest value in input is < 10000, 
-	// so num strings can only be up to 4 chars
-	int max_len = 4;
+	std::vector<std::vector<int>> nums;
+	std::vector<int> ops;
 	long long total{};
 
 	// Extract number rows and operations row
 	while (std::getline(file, row)) {
-    	std::vector<std::string> r;
+    	std::vector<int> r;
     	std::stringstream ss(row);
     	// >> grabs segments of the stream, skipping whitespace
     	while (ss>>seg){
     		// If segment is only 1 char (* or +), shift it to 0,1
-    		if (seg[0] =='*' || seg[0] =='+') r.push_back(seg);
-    		else {
-    			seg.insert(0,max_len - seg.length(), '0');
-    			r.push_back(seg);
-    		}
+    		if (seg[0] =='*' || seg[0] =='+') r.push_back(seg[0]-42);
+    		else r.push_back(std::stoi(seg));
     	}
     	// If parsing operation line, set vector to ops
     	if (!(row[0] == '*' || row[0] =='+')) nums.push_back(r);
@@ -40,11 +34,11 @@ int main() {
    	// Calculate expressions
    	for (size_t op = 0; op < ops.size(); ++op){
    		// Convert operation to bool, add being 1 is truthy
-   		bool add = ops[op] == "+";
+   		bool add = ops[op];
    		// Set column total to 1 for multiplication
    		long long coltot = 1;
    		for (size_t n=0; n<nums.size(); ++n){
-   			coltot = (add) ? coltot+std::stoi(nums[n][op]) : coltot * std::stoi(nums[n][op]);
+   			coltot = (add) ? coltot+nums[n][op] : coltot * nums[n][op];
    		}
    		// Subtract initial 1
    		if (add) coltot--;
